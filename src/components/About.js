17156f2cf01a65b8e5447b7e1ef4099d2f481ce6
Prajwalgawande <86409436+Prajwalgawande@users.noreply.github.com>
2022-01-16 09:14:29 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import './about.css'
 
 export default function About() {
     return (
-        <div className='about'>
+        <div id="about" className='about'>
             <h3 className="abouthead">
                 ABOUT US
             </h3>
